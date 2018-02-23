1d836efad7d714c2c8cf84d730ef91866db3000c
Hoon Lee <yl.hoony@gmail.com>
2018-02-23 19:56:30 +0000

    automatically backed up by learn

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -1,15 +1,25 @@
 class AttractionsController < ApplicationController
 
   def index
-    
+
   end
 
   def new
-    
+
   end
 
   def create
-    
+
   end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
   
+
 end
