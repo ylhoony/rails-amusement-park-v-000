7d78e40c7114ae0d0ea3b497d887542a139bed83
Hoon Lee <yl.hoony@gmail.com>
2018-02-23 21:27:46 +0000

    automatically backed up by learn

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -2,11 +2,11 @@ class RidesController < ApplicationController
 
   def new
     binding.pry
-    @ride = Ride.new
-    @ride.user = User.find(params[:user_id])
-    @ride.attraction = Attraction.find(params[:attraction_id])
-    @ride.save
-    @ride.take_ride
+    ride = Ride.new
+    ride.user = User.find(params[:user_id])
+    ride.attraction = Attraction.find(params[:attraction_id])
+    ride.save
+    ride.take_ride
     render user_path(@ride.user)
   end
 
