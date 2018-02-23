799697b80e24b302897986f3e447de1de4024e82
Hoon Lee <yl.hoony@gmail.com>
2018-02-23 22:03:12 +0000

    automatically backed up by learn

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -1,7 +1,6 @@
 class RidesController < ApplicationController
 
   def new
-    # binding.pry
     @ride = Ride.new
     @ride.user = User.find(params[:user_id])
     @ride.attraction = Attraction.find(params[:attraction_id])
