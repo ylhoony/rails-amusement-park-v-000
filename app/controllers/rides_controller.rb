774087f6e1d49cac22ede24550bb2ef5cf8b9d36
Hoon Lee <yl.hoony@gmail.com>
2018-02-23 21:50:24 +0000

    automatically backed up by learn

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -6,9 +6,11 @@ class RidesController < ApplicationController
     @ride.user = User.find(params[:user_id])
     @ride.attraction = Attraction.find(params[:attraction_id])
     @ride.save
-    @ride.take_ride
-    binding.pry
-    redirect_to user_path(@ride.user), notice: @ride.take_ride
+    if @ride.take_ride
+      redirect_to user_path(@ride.user), notice: "Thanks for riding the #{@ride.attraction.name}!"
+    else
+      redirect_to user_path(@ride.user), notice: @ride.take_ride
+    end
   end
 
 end
